4f8a9a7062b70826325a2efdb4140b28d9a050be
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2019-04-30 19:33:10 -0400

    Refactored components

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
+import Features from './components/Features';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+describe('Component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('renders the UI as expected', () => {
+    const tree = renderer
+    .create(<div/>)
+    .toJson();
+    expect(tree).toMatchSnapshot();
+  })
+
+})
+
